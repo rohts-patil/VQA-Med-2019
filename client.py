78c18dafed96c607d50b55d942592e0986e23910
Rohit Patil <rohts.patil@gmail.com>
2020-06-01 11:59:34 +0530

    added streamlit app as client

--- client.py
+++ client.py
@@ -1,23 +1,21 @@
+import streamlit as st
+from PIL import Image
 import json
-
 import numpy as np
 import requests
-from PIL import Image
 
-# text = "Which organ is shown in scan?"
-# text = "what modality is shown?"
-# text = "what type of contrast did this patient have?"
-# text = "what kind of scan iresponse.texts this?"
-# text = "what imaging plane is depicted here?"
-# text = "what part of the body does this x-ray show?"
-# text = "does this image look normal?"
-text = "what part of the body is being imaged?	"
-# url = "http://127.0.0.1:5000/predict_question_type"
-url = "http://127.0.0.1:5000/predict_answer"
-image_bytes = Image.open(
-    "C:\\Users\\rohit.patil\\Downloads\\VQA-20200529T125140Z-001\\val\\Val_images\\synpic35681.jpg"
-)
-image_bytes = json.dumps(np.array(image_bytes).tolist())
-response = requests.post(url, json={"question": text, "image": image_bytes})
-parsed = json.loads(response.text)
-print(json.dumps(parsed, indent=2))
+st.title("VAQ Medical 2019")
+question = st.text_input("Question Box", "Which type of organ is shown in scan?")
+uploaded_file = st.file_uploader("Please choose an image.", type="jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', width=200, use_column_width=False)
+    st.write("")
+    st.markdown("### Preparing the answer to your question...")
+    url = "http://127.0.0.1:5000/predict_answer"
+
+    image = json.dumps(np.array(image).tolist())
+    response = requests.post(url, json={"question": question, "image": image})
+    parsed = json.loads(response.text)
+    print(json.dumps(parsed, indent=2))
+    st.markdown('### The answer is:- %s' % (parsed['answer']))
